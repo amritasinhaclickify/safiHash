381a28a3eaf04546bea1b0a34d45dc2af7e44b3c
amritasinha01 <amritasinha.vishal@gmail.com>
2025-10-29 14:44:49 +0530

    Fixed backendURL to Render domain in all chatbot scripts

--- static/js/chatbot.js
+++ static/js/chatbot.js
@@ -2,7 +2,7 @@
 // Merged version: JWT-secure chat, KYC prompts, notifications, and simple JSON-paste support.
 
 (async function () {
-  const backendURL = window.backendURL || 'http://127.0.0.1:5000';
+  const backendURL = window.backendURL || 'https://safihash.onrender.com';
 
   // Helpers
   function el(sel) { return document.querySelector(sel); }
